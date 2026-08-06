1432a3e7d7e70def7a6aedbae4e6aa8dfb5b6856
svaa-dev <sotomayorvalerioadrian@gmail.com>
2026-08-06 00:13:17 -0500

    feat: custom fonts and updated configuration

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -11,7 +11,7 @@ export default {
   ],
   experimentalTernaries: false,
   experimentalOperatorPosition: 'end',
-  printWidth: 80,
+  printWidth: 120,
   tabWidth: 2,
   useTabs: false,
   semi: false,
